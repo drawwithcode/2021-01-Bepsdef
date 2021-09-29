cbe98162bddae4c6d51ee20e4826d014a1b69151
Bepsdef <giuseppedefilippis23@gmail.com>
2021-09-29 09:08:01 +0200

    delivery 30.09

--- sketch.js
+++ sketch.js
@@ -1,13 +1,53 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  angleMode(DEGREES);
+  background("black");
+
+  slider = createSlider(10, 500, 0);
+  slider.position(20, 50);
+  slider.style("width", "170px");
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  noStroke();
+  fill("white");
+  textSize(16);
+  text("drag it until you make it", 25, 40);
+
+  push();
+  translate(width / 2, height / 2);
+  stroke(mouseX, mouseX, 255);
+  noFill();
+  rotate(frameCount / 4);
+  line(0, 0, slider.value(), 10);
+  pop();
+
+  push();
+  translate(width / 2, height / 2);
+  stroke(mouseX, mouseX, 255);
+  noFill();
+  rotate(frameCount * 4);
+  circle(0, 50, 400);
+  circle(0, -50, 400);
+  pop();
+
+  push();
+  rotate(frameCount / 2);
+  translate(width / 2, height / 2);
+  stroke("black");
+  line(-2000, 0, 2000, 0);
+  rotate(45);
+  line(0, -2000, 0, 2000);
+  pop();
+
+  push();
+  stroke("white");
+  translate(width / 2, height / 2);
+  line(-2000, 0, 2000, 0);
+  line(0, -2000, 0, 2000);
+  pop();
+}
+function doubleClicked() {
+  randomcolor = random(0, 255);
+  background(randomcolor);
 }
